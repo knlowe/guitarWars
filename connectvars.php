6781546c393b4fa6e722abb71e223301a12e6fd8
kieran <mistyassasin@gmail.com>
2021-03-08 13:27:37 +0000

    pushing a working copy of guitarWars to git

---
+++ connectvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'knlowe');
+  define('DB_PASSWORD', 'YmjsE9SL');
+  define('DB_NAME', 'knlowe');
+?>
